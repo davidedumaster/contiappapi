77288d0d101a1b570a583b369d8522b417ba1dff
David Eduardo Ramirez Soto <david.ramirez@bancontinental.com.py>
2024-06-06 14:48:46 -0400

    ajuste menor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("com.github.davidedumaster:contiappapi:0.0.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
